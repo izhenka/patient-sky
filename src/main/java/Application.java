93ac2cd03433830e2c8cf4ef11303cebb5dfe67b
evgeniagurova <evgenia.gurova@unit.no>
2021-04-09 21:10:39 +0200

    Lesing fra fil

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,26 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+
 public class Application {
     public static void main(String[] args) {
-
         System.out.println("Hello!");
+        readFileToJSONObject("calendar_data/Danny boy.json");
+    }
 
+
+    public static void readFileToJSONObject(String filename) {
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader(filename));
+            JSONObject jsonObject = (JSONObject) obj;
+            System.out.println(jsonObject);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
+
 }
